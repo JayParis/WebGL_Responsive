a9c6f77ed22576ba74ff210715686699ac0ffae3
JayParis <jay.paris32@gmail.com>
2023-07-30 15:00:52 +0100

    Obj parse start

--- renderer.js
+++ renderer.js
@@ -55,12 +55,105 @@ async function loadShadersAndRunDemo(){
         .then(result => result.text());
     const downsampleShaderText = await fetch('./Shaders/downsample_fragment.glsl')
         .then(result => result.text());
+
+    const planeObjText = await fetch('./OBJ_BlenderPlane.obj')
+        .then(result => result.text());
     
+
     hasInit = true;
-    RunDemo(vertexShaderText,fragmentShaderText,downsampleShaderText);
+    RunDemo(vertexShaderText,fragmentShaderText,downsampleShaderText, planeObjText);
 }
 
-var RunDemo = function(vertexShaderText, fragmentShaderText, downsampleShaderText){
+function parseOBJ(text) {
+    // because indices are base 1 let's just fill in the 0th data
+    const objPositions = [[0, 0, 0]];
+    const objTexcoords = [[0, 0]];
+    const objNormals = [[0, 0, 0]];
+  
+    // same order as `f` indices
+    const objVertexData = [
+      objPositions,
+      objTexcoords,
+      objNormals,
+    ];
+  
+    // same order as `f` indices
+    let webglVertexData = [
+      [],   // positions
+      [],   // texcoords
+      [],   // normals
+    ];
+  
+    function newGeometry() {
+      // If there is an existing geometry and it's
+      // not empty then start a new one.
+      if (geometry && geometry.data.position.length) {
+        geometry = undefined;
+      }
+      setGeometry();
+    }
+  
+    function addVertex(vert) {
+      const ptn = vert.split('/');
+      ptn.forEach((objIndexStr, i) => {
+        if (!objIndexStr) {
+          return;
+        }
+        const objIndex = parseInt(objIndexStr);
+        const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+        webglVertexData[i].push(...objVertexData[i][index]);
+      });
+    }
+  
+    const keywords = {
+      v(parts) {
+        objPositions.push(parts.map(parseFloat));
+      },
+      vn(parts) {
+        objNormals.push(parts.map(parseFloat));
+      },
+      vt(parts) {
+        // should check for missing v and extra w?
+        objTexcoords.push(parts.map(parseFloat));
+      },
+      f(parts) {
+        const numTriangles = parts.length - 2;
+        for (let tri = 0; tri < numTriangles; ++tri) {
+          addVertex(parts[0]);
+          addVertex(parts[tri + 1]);
+          addVertex(parts[tri + 2]);
+        }
+      },
+    };
+  
+    const keywordRE = /(\w*)(?: )*(.*)/;
+    const lines = text.split('\n');
+    for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+      const line = lines[lineNo].trim();
+      if (line === '' || line.startsWith('#')) {
+        continue;
+      }
+      const m = keywordRE.exec(line);
+      if (!m) {
+        continue;
+      }
+      const [, keyword, unparsedArgs] = m;
+      const parts = line.split(/\s+/).slice(1);
+      const handler = keywords[keyword];
+      if (!handler) {
+        continue;
+      }
+      handler(parts, unparsedArgs);
+    }
+  
+    return {
+      position: webglVertexData[0],
+      texcoord: webglVertexData[1],
+      normal: webglVertexData[2],
+    };
+}
+
+var RunDemo = function(vertexShaderText, fragmentShaderText, downsampleShaderText, planeObjText){
     var canvas = document.getElementById('application');
     var gl = canvas.getContext('webgl2');
     if(!gl) { console.log("WebGL not supported, falling back on experimental"); gl = canvas.getContext('experimental-webgl'); }
@@ -117,6 +210,9 @@ var RunDemo = function(vertexShaderText, fragmentShaderText, downsampleShaderTex
     const topAndBottomLoc = gl.getUniformLocation(prg_2, 'topAndBottom'); const main_sampler2Loc = gl.getUniformLocation(prg_2, 'sampler_2');
     //const sampler2Loc = gl.getUniformLocation(prg_1, 'sampler_2');
 
+    const meshData = parseOBJ(planeObjText);
+    console.log(meshData);
+
     var planeVertices = 
 	[ // X, Y, Z
 		// Front
@@ -125,6 +221,8 @@ var RunDemo = function(vertexShaderText, fragmentShaderText, downsampleShaderTex
 		-1.0, -1.0, 1.0,    
 		-1.0, 1.0, 1.0,     
 	];
+
+    //planeVertices = meshData[0];
 
     var planeTexCoords = 
     [ // U V
